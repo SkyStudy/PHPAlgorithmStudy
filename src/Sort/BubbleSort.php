d6bf00917780aac1ffc4ee031b8add8bedaee9e6
ReenExe <ipourri@gmail.com>
2016-05-05 21:23:29 +0300

    very fast and hardCode bubble sort

--- src/Sort/BubbleSort.php
+++ src/Sort/BubbleSort.php
@@ -4,8 +4,18 @@ namespace SkyStudy\Algorithm\Sort;
 
 class BubbleSort
 {
-    public function sort(array $sort)
+    public function sort(array $source)
     {
+        $count = count($source);
+        
+        for ($i = 0; $i < $count; ++$i) {
+            for ($j = $i + 1; $j < $count; ++$j) {
+                if ($source[$i] > $source[$j]) {
+                    list($source[$i], $source[$j]) = [$source[$j], $source[$i]];
+                }
+            }
+        }
 
+        return $source;
     }
 }
