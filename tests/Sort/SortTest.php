9baef2d9105a781482548589cad4cabee38d2eff
ReenExe <ipourri@gmail.com>
2016-05-05 20:57:15 +0300

    default sort example

--- tests/Sort/SortTest.php
+++ tests/Sort/SortTest.php
@@ -4,6 +4,11 @@ class SortTest extends \PHPUnit_Framework_TestCase
 {
     public function test()
     {
+        $source = [3, 2, 1];
+        $expected = [1, 2, 3];
+
+        sort($source);
         
+        $this->assertSame($expected, $source);
     }
 }
