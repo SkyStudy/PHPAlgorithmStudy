d8868c18c1d86fb9abf7d638d5486107efd57232
SkyInterBy <soJIo1010@mail.ru>
2016-05-29 15:26:37 +0300

    remove debug

--- src/Sort/InsertionSort.php
+++ src/Sort/InsertionSort.php
@@ -2,14 +2,10 @@
 
 namespace SkyStudy\Algorithm\Sort;
 
-use SkyStudy\Algorithm\Dumper\ChangeStateArrayDumper;
-
 class InsertionSort
 {
     public function sort(array $source)
     {
-        $dumper = new ChangeStateArrayDumper();
-        
         $count = count($source);
 
         for ($i = 1; $i < $count; ++$i) {
@@ -17,16 +13,12 @@ class InsertionSort
 
             $j = $i - 1;
             while ($j >= 0 && $source[$j] > $insert) {
-                $dumper->dump($source);
                 $source[$j + 1] = $source[$j];
-                $dumper->dump($source);
                 --$j;
             }
             $source[$j + 1] = $insert;
-            $dumper->dump($source);
         }
 
         return $source;
     }
-
 }
