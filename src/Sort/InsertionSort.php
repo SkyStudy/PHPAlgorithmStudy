7c6d4a9af0167f818583ebec60bc893db83dd83c
SkyInterBy <soJIo1010@mail.ru>
2016-05-29 14:52:19 +0300

    pass tests

--- src/Sort/InsertionSort.php
+++ src/Sort/InsertionSort.php
@@ -4,8 +4,21 @@ namespace SkyStudy\Algorithm\Sort;
 
 class InsertionSort
 {
-    public function sort(array $sort)
+    public function sort(array $source)
     {
+        $count = count($source);
 
+        for ($i = 1; $i < $count; ++$i) {
+            $insert = $source[$i];
+
+            $j = $i - 1;
+            while ($j >= 0 && $source[$j] > $insert) {
+                $source[$j + 1] = $source[$j];
+                --$j;
+            }
+            $source[$j + 1] = $insert;
+        }
+
+        return $source;
     }
 }
