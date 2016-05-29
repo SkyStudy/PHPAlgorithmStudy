84cafa9306ac60e507f5f3fc38e63c49d95bbe84
SkyInterBy <soJIo1010@mail.ru>
2016-05-29 15:03:00 +0300

    dump version

--- src/Sort/InsertionSort.php
+++ src/Sort/InsertionSort.php
@@ -13,12 +13,25 @@ class InsertionSort
 
             $j = $i - 1;
             while ($j >= 0 && $source[$j] > $insert) {
+                $this->dump($source);
                 $source[$j + 1] = $source[$j];
+                $this->dump($source);
                 --$j;
             }
             $source[$j + 1] = $insert;
+            $this->dump($source);
         }
 
         return $source;
+    }
+
+    private function dump(array $array)
+    {
+        $string = json_encode($array);
+        echo <<<CLI
+
+$string
+
+CLI;
     }
 }
