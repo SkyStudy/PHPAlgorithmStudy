294a5046422aa09463d6c35d2865f570d40c0f77
SkyInterBy <soJIo1010@mail.ru>
2016-05-29 15:50:53 +0300

    progress

--- src/Sort/QuickSort.php
+++ src/Sort/QuickSort.php
@@ -17,13 +17,14 @@ class QuickSort
         $j = $r;
         $x = $array[($l + $r) / 2];
         do {
-            while ($array[$i] < $x) $i++;
-            while ($array[$j] > $x) $j--;
+            while ($array[$i] < $x) ++$i;
+            while ($array[$j] > $x) --$j;
             if ($i <= $j) {
-                if ($array[$i] > $array[$j])
+                if ($array[$i] > $array[$j]) {
                     list($array[$i], $array[$j]) = array($array[$j], $array[$i]);
-                $i++;
-                $j--;
+                }
+                ++$i;
+                --$j;
             }
         } while ($i <= $j);
         if ($i < $r) $this->inner($array, $i, $r);
