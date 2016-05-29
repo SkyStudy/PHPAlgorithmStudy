2a493810daeaec80b0bbbd8b55884ff8ff97fafd
SkyInterBy <soJIo1010@mail.ru>
2016-05-29 15:25:57 +0300

    progress

--- src/Dumper/ChangeStateArrayDumper.php
+++ src/Dumper/ChangeStateArrayDumper.php
@@ -4,8 +4,16 @@ namespace SkyStudy\Algorithm\Dumper;
 
 class ChangeStateArrayDumper extends ArrayDumper
 {
+    private $state;
+
     public function dump(array $array)
     {
+        if ($this->state === $array) {
+            return;
+        }
+
+        $this->state = $array;
+
         parent::dump($array);
     }
 }
